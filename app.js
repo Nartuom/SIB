22396a787cd0ed324bf386230c46d10cfff84ddd
Nartuom <thomas.burton.lawl@gmail.com>
2020-09-02 00:00:01 +0100

    add pagination and removed padding and margin on images

--- app.js
+++ app.js
@@ -10,7 +10,61 @@ const { Submission } = require("snoowrap");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+let first;
+let last;
+let pageNumber = 0;
+let postObj = function (post) {
+  return {
+    title: post["title"],
+    author: post["author"],
+    url: post["url"],
+    id: post["id"],
+  };
+};
+//App Routes
 app.get("/", function (req, res) {
+  const data = [];
+  snoowrap
+    .fromApplicationOnlyAuth({
+      userAgent: "tomblScrape",
+      clientId: process.env.CLIENT_ID,
+      deviceId: process.env.DEVICE_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN,
+    })
+    .then((r) => {
+      if (pageNumber == 0) {
+        return r
+          .getSubreddit("spaceporn")
+          .getTop({ time: "week", limit: 9 })
+          .then((posts) => {
+            // do something with posts from the front page
+            posts.forEach(function (post) {
+              data.push(postObj(post));
+            });
+            first = data[0].id;
+            last = data[8].id;
+            res.render("index", { topPosts: data, pageNumber: pageNumber });
+          });
+      } else {
+        return r
+          .getSubreddit("spaceporn")
+          .getTop({ time: "week", before: `t3_${first}`, limit: 9 })
+          .then((posts) => {
+            posts.forEach(function (post) {
+              data.push(postObj(post));
+            });
+            pageNumber--;
+            first = data[0].id;
+            last = data[8].id;
+            res.render("index", { topPosts: data, pageNumber: pageNumber });
+          });
+      }
+    });
+});
+
+app.get("/next-page", function (req, res) {
   const data = [];
   snoowrap
     .fromApplicationOnlyAuth({
@@ -25,19 +79,16 @@ app.get("/", function (req, res) {
       // Now we have a requester that can access reddit through a "user-less" Auth token
       return r
         .getSubreddit("spaceporn")
-        .getTop({ time: "week", limit: 50 })
+        .getTop({ time: "week", after: `t3_${last}`, limit: 9 })
         .then((posts) => {
           // do something with posts from the front page
           posts.forEach(function (post) {
-            console.log(post.id);
-            data.push({
-              title: post["title"],
-              author: post["author"],
-              url: post["url"],
-              id: post["id"],
-            });
+            data.push(postObj(post));
           });
-          res.render("index", { topPosts: data });
+          pageNumber++;
+          first = data[0].id;
+          last = data[8].id;
+          res.render("index", { topPosts: data, pageNumber: pageNumber });
         });
     });
 });
@@ -62,5 +113,3 @@ if (url.USERDOMAIN == "MARVIN") {
     console.log("Server Live at " + url.IP);
   });
 }
-
-module.exports = app;
